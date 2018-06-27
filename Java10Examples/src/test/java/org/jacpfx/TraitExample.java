8fd980fccb38bb1155c7ccdc75c2639c5b37dd2b
Andy Moncsek <amo.ahcp@gmail.com>
2018-06-27 11:17:25 +0200

    initial commit

---
+++ Java10Examples/src/test/java/org/jacpfx/TraitExample.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright [2018] [Andy Moncsek]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jacpfx;
+
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class TraitExample {
+
+  @Test
+  public void testPseudotraits() {
+    Superfruit myFruit = new MySuperfruit("OrangeBanana","vitamin c", "vitamin b", "vitamin d");
+    var friut = (SuperfruitDelegate & Banana & Orange) () -> myFruit;
+    System.out.println("This is the fruit: "+friut.name()+" it contains vitamin c: "+friut.containsVitaminC() +" and vitamin b: "+friut.containsVitaminB());
+  }
+
+}
+
+class MySuperfruit implements Superfruit{
+  String name;
+  String[] vitamins;
+  public MySuperfruit(String name, String ...vitamins) {
+    this.name = name;
+    this.vitamins = vitamins;
+  }
+
+  @Override
+  public String name() {
+    return this.name;
+  }
+
+  @Override
+  public List<String> getVitamins() {
+    return Arrays.asList(this.vitamins);
+  }
+}
+
+
+interface Superfruit {
+  String name();
+  List<String> getVitamins();
+}
+
+interface SuperfruitDelegate extends Superfruit{
+  Superfruit instance();
+  default String name() {
+    return instance().name();
+  }
+  default List<String>getVitamins() {
+    return instance().getVitamins();
+  }
+}
+
+interface Banana extends Superfruit{
+  String VITAMIN = "vitamin c";
+
+  default boolean containsVitaminC() {
+    return getVitamins().contains(VITAMIN);
+  }
+}
+
+interface Orange extends Superfruit{
+  String VITAMIN = "vitamin b";
+
+  default boolean containsVitaminB() {
+    return getVitamins().contains(VITAMIN);
+  }
+}
